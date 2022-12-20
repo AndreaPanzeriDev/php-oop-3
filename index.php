8030df823d49fdd638dfb7b8ff65220020b05511
AndreaPanzerDev <andpanz@libero.it>
2022-12-20 18:30:28 +0100

    .

--- index.php
+++ index.php
@@ -7,13 +7,20 @@ include_once __DIR__ . '/classi/audiolibro.php';
 
 $generi = [
   'giallo' => new Generi('giallo', 'icon-font-awesome-giallo'),
-  'horror' => new Generi('horror', 'icon-font-awesome-horror')
+  'horror' => new Generi('horror', 'icon-font-awesome-horror'),
+  'jazz' => new Generi('jazz','icona-horror'),
+  'rock'   => new Generi('rock','icona-rock')
+
 ];
 var_dump( $generi );
 
 $prodotti = [
   new Libro( 'La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 250, 'flessibile' ),
   new audioLibro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 120, 'spotify', 'https://spotify-libri.com'),
+  new Cd('titolo','autore',29.90,$generi['jazz'],true,'copertina',10 ),
+  new Cd('titolo','autore',25.99,$generi['rock'],true,'copertina',20 ),
+  new Dvd('titolo','autore',25.99,$generi['giallo'],true,'copertina',120,'MPEG'),
+  new Dipendenti('nome','immagine','ruolo',20,'maschio')
 ];
 
 var_dump( $prodotti );
